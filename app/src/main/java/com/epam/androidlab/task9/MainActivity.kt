75922b4d11dae3a3e7096061398d3ccc5fc5e104
AndrewPolovko <AndrewPolovko@gmail.com>
2017-05-31 23:28:12 +0300

    Initial commit

---
+++ app/src/main/java/com/epam/androidlab/task9/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.epam.androidlab.task9
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
